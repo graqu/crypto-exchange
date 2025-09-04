b13b4b9361741235bd71e304473e91cdae5d4c6a
Jacek <jacek_135@yahoo.pl>
2025-09-04 03:13:02 +0200

    Configure global theme variables file

---
+++ packages/ui/src/lib/container/container.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'lib-container',
+  template: `<div ngClass="class()"><ng-content></ng-content></div>`,
+  styles: [
+    `
+      div {
+        box-sizing: border-box;
+        width: min(50vw, 27rem);
+        margin: 0 auto;
+        padding: 0;
+      }
+    `,
+  ],
+})
+export class ContainerComponent {
+  class = input<string>('');
+}
